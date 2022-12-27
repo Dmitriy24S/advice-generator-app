98021cde876cf5dcad9254426efbaf32bc653641
Dmitriy24S <devilins24@gmail.com>
2022-12-27 16:18:49 +0200

    style: height: 100svh - mobile content centering

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import Advice from './components/Advice'
 
 function App() {
   return (
-    <div className='App bg-gray-blue-dark min-h-screen p-4 text-center flex flex-col items-center justify-center'>
-      <div className='card relative bg-gray-blue-medium rounded-lg max-w-[444px] mt-4  p-8 font-extrabold min-h-[150px]'>
+    <div className='App bg-gray-blue-dark p-4 text-center flex flex-col items-center justify-center'>
+      <div className='card relative bg-gray-blue-medium rounded-lg max-w-[444px] mt-4 p-8 font-extrabold min-h-[150px]'>
         <Advice />
       </div>
     </div>
